06affd88369fbcbf7c78b422a07fb62b65a235d6
Zhou Shang <alex.zhou.shang>
2019-02-27 10:04:01 -0800

    first

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,10 @@
+// module.exports = {
+// 	mongoURI: 'mongodb://admin:admin1@ds255005.mlab.com:55005/web-ref-app)',
+// 	secretOrKey: 'secret',
+// };
+
 module.exports = {
-	mongoURI: 'mongodb://admin:admin1@ds255005.mlab.com:55005/web-ref-app)',
-	secretOrKey: 'secret'
-};
+    db: 'mongodb://localhost:27017/swipe',
+    db_dev: 'mongodb://url:port/db',
+    secretOrKey: 'secret',
+  };
